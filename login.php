1a30833ca120ea01dbaf0543f1ec3eb46fec31a0
2000jedi <2000jedi@gmail.com>
2016-09-15 13:18:58 +0800

    Edit on login.php
    Rewrite the material design into our designer’s work. Partially done
    and the background image is not prepared yet.

--- login.php
+++ login.php
@@ -41,6 +41,7 @@ if ( isset($_POST['username']) and isset($_POST['password']) ){
     <link rel="stylesheet" href="/framework/material/material.min.css">
     <link rel="stylesheet" href="/framework/material/material-dashboard-styles.css">
     <link rel="stylesheet" href="/framework/geodesic/base.css">
+    <link rel="stylesheet" href="/framework/sam/login.css">
 </head>
 <body>
 <script src="/framework/js/jq.js"></script>
@@ -57,37 +58,36 @@ if ( isset($_POST['username']) and isset($_POST['password']) ){
         }
     }
 </script>
-    <header class="demo-header mdl-layout__header mdl-color--white mdl-color--grey-100 mdl-color-text--grey-600" style="display: flex">
-        <div class="mdl-layout__header-row">
-            <span id="title" class="mdl-layout-title"><?= $appName ?></span>
-        </div>
-    </header>
     <form id="form" action="login.php" method="post">
-        <div class="demo-cards mdl-cell mdl-grid mdl-grid--no-spacing" style="margin: 1em auto; display: block">
-            <div class="demo-updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--6-col mdl-cell--6-col-tablet mdl-cell--12-col-desktop">
-                <div class="mdl-card__title mdl-card--expand mdl-color--teal-300">
-                    <h2 class="mdl-card__title-text">Login</h2>
+        <div id="request">
+            <div id="title">
+                Welcome
+            </div>
+            <div class="mdl-card__supporting-text">
+                <div class="input-box">
+                    <label class="func" id="user" for="usnmInput">Your username</label>
+                    <input name="username" type="text" id="usnmInput" onfocus="$('#user.func').css('display','none')" onblur="$('#user.func').css('display','inherit')"/>
+
                 </div>
-                <div class="mdl-card__supporting-text">
-                    <div><?=$msg ?></div>
-                    <div class="mdl-textfield mdl-js-textfield">
-                        <input class="mdl-textfield__input" name="username" type="text" id="usnmInput" />
-                        <label class="mdl-textfield__label" for="usnmInput">Your username</label>
-                    </div>
-                    <div class="mdl-textfield mdl-js-textfield">
-                        <input class="mdl-textfield__input" name="password" type="password" id="pswdInput"/>
-                        <label class="mdl-textfield__label" for="pswdInput">Your password</label>
-                    </div>
-                    <input type="submit" style="display: none">
+                <div class="input-box">
+                    <label class="func" id="pass" for="pswdInput">Your password</label>
+                    <input name="password" type="password" id="pswdInput" onfocus="$('#pass.func').css('display','none')" onblur="$('#pass.func').css('display','inherit')"/>
                 </div>
-                <div class="mdl-card__actions mdl-card--border">
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" onclick="$('#form').submit()">
-                        Sign In
-                    </a>
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" onclick="forgotPassword()">
-                        Forgot Password
-                    </a>
-                </div>
+                <input type="submit" style="display: none">
+            </div>
+            <hr />
+            <div class="buttons" align="center">
+                <a class="button" id="sign-in" style="margin-right: 1em" onclick="$('#form').submit()">
+                    SIGN IN
+                </a>
+                <a class="button" id="forget-password" onclick="forgotPassword()">
+                    FORGOT PASSWORD
+                </a>
+            </div>
+        </div>
+        <div id="hint-box">
+            <div id="hint">
+                <?=$msg ?>
             </div>
         </div>
     </form>
