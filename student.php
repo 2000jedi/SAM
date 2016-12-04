49998e04d9db11c9250c59a4352d9605375bda1b
2000jedi <2000jedi@gmail.com>
2016-12-04 12:54:06 +0800

    Delete redundant codes
    To retrieve the codes, get them from SAM’s branch

--- student.php
+++ student.php
@@ -33,25 +33,6 @@ if (!function_exists('checkForceQuit')){
     <script src="/framework/js/form.js"></script>
     <script src="/framework/js/masonry.js"></script>
     <script>
-        function toggleModules(id){
-            $('#right-part').hide();
-            $('#mHome').hide();
-            $('#left-tab-Home').css("background","").css("color","#eceff1");
-            $('#mClasses').hide();
-            $('#left-tab-Classes').css("background","").css("color","#eceff1");
-            $('#mActivities').hide();
-            $('#left-tab-Activities').css("background","").css("color","#eceff1");
-            $('#mColleges').hide();
-            $('#left-tab-Colleges').css("background","").css("color","#eceff1");
-            $('#mPresentations').hide();
-            $('#left-tab-Presentations').css("background","").css("color","#eceff1");
-            $('#mSettings').hide();
-            $('#left-tab-Settings').css("background","").css("color","#eceff1");
-            $('#m'+id).show();
-            $('#left-tab-'+id).css("background","#00BCD4").css("color","#37474F");
-            $('#title').html(id);
-            $('.demo-drawer').removeClass("is-visible");
-        }
         <?php
             require $_SERVER['DOCUMENT_ROOT']."/template/scripts/UID.php";
             require $_SERVER['DOCUMENT_ROOT']."/template/scripts/base.js";
@@ -64,19 +45,76 @@ if (!function_exists('checkForceQuit')){
             require $_SERVER['DOCUMENT_ROOT']."/template/scripts/college.js";
             require $_SERVER['DOCUMENT_ROOT']."/template/scripts/presentation.js";
         ?>
+
+        function updatePercentage(perc) { // draw assignment percentage circle
+            perc = parseFloat(perc);
+            em = Number(getComputedStyle(document.body, null).fontSize.replace(/[^\d]/g, ''));
+
+            var canvas = document.getElementById("percentage");
+            var ctx = canvas.getContext("2d");
+            var text = parseInt(perc * 100) + '%';
+
+            canvas.width = 40 * em;
+            canvas.height = 40 * em;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.lineWidth = 25;
+
+            if (perc == 1.0) {
+                ctx.beginPath();
+                ctx.arc(20 * em, 20 * em, 12 * em, 0, 2 * Math.PI);
+                ctx.strokeStyle = 'rgba(81,157,217,1)';
+                ctx.stroke();
+            }
+
+            if (perc == 0.0) {
+                ctx.beginPath();
+                ctx.arc(20 * em, 20 * em, 12 * em, 0, 2 * Math.PI);
+                ctx.strokeStyle = 'rgba(240,124,120,1)';
+                ctx.stroke();
+            }
+
+            if ((perc < 1) && (perc > 0)) {
+                ctx.beginPath();
+                ctx.arc(20 * em, 20 * em, 12 * em, (1.0125 - perc) * 2 * Math.PI, 2 * Math.PI - 0.075);
+                ctx.strokeStyle = 'rgba(81,157,217,1)';
+                ctx.stroke();
+
+                ctx.beginPath();
+                ctx.strokeStyle = 'rgba(240,124,120,1)';
+                ctx.arc(20 * em, 20 * em, 12 * em, -0.9875 * 2 * Math.PI, -(perc + 0.0125) * 2 * Math.PI);
+                ctx.stroke();
+            }
+
+            ctx.font = 8 * em + "px Arial";
+            ctx.fillStyle = 'rgba(133,189,234,1)';
+            ctx.fillText(text, (canvas.width - text.length*em*4)/2 - 25, (canvas.height + 4*em)/2);
+        }
+
+        // toggle modules
+
         var flag_showApps = false;
         function showApps(){
             flag_showApps = !flag_showApps;
             if (flag_showApps){
                 $('#apps-menu-detail').css('display','block');
-                $('#apps-menu-arrow-front').css('display','block');
-                $('#apps-menu-arrow-back').css('display','block');
             }
             else{
                 $('#apps-menu-detail').css('display','none');
-                $('#apps-menu-arrow-front').css('display','none');
-                $('#apps-menu-arrow-back').css('display','none');
             }
+        }
+        function showModule(id){
+            $('#assignment').css('display','none');
+            $('#classes').css('display','none');
+            $('#settings').css('display','none');
+
+            $('#app-assignment').css('display','inline-block');
+            $('#app-classes').css('display','inline-block');
+            $('#app-settings').css('display','inline-block');
+
+            $('#' + id).css('display', 'block');
+            $('#app-' + id).css('display', 'none');
+
+            showApps();
         }
 </script>
 </head>
@@ -93,26 +131,24 @@ if (!function_exists('checkForceQuit')){
         <img id="avatar" src="/framework/material-images/user.png" height="40px">
         <p id="right-banner" onclick="showUserPanel()">
     </div>
-    <div id="apps-menu-arrow-back" style="display: none;"></div>
-    <div id="apps-menu-arrow-front" style="display: none;"></div>
     <div id="apps-menu-detail" style="display: none;" aria-label="Apps" aria-hidden="false" role="region">
         <ul class="list-apps" aria-dropeffect="move">
             <li class="drag" style="display: none;" id="app-assignment">
-                <a class="app" onclick="showAssignment()">
+                <a class="app" onclick="showModule('assignment')">
                     <span class="app-img" style="background-image: url('/files/icons/assignments.svg');background-size: 64px 64px;"></span>
                     <span class="app-desc"><div style="margin: 0 auto;">Assignments</div></span>
                 </a>
             </li>
 
             <li class="drag" style="display: inline-block;" id="app-classes">
-                <a class="app" onclick="showClasses()">
+                <a class="app" onclick="showModule('classes')">
                     <span class="app-img" style="background-image: url('/files/icons/classes.svg');background-size: 64px 64px;"></span>
                     <span class="app-desc"><div style="margin: 0 auto;">Classes</div></span>
                 </a>
             </li>
 
             <li class="drag" style="display: inline-block;" id="app-settings">
-                <a class="app" onclick="showSettings()">
+                <a class="app" onclick="showModule('settings')">
                     <span class="app-img" style="background-image: url('/files/icons/setting.svg');background-size: 64px 64px;"></span>
                     <span class="app-desc"><div style="margin: 0 auto;">Settings</div></span>
                 </a>
@@ -176,12 +212,12 @@ if (!function_exists('checkForceQuit')){
                 <div class="" >
                     <h2 class="c">More</h2>
                     <div class="">Computerization Proudly Present</div>
-                    <div class="">Version                16.11.06</div>
+                    <div class="">Version 16.11.23</div>
                     <a class="a-button" onclick="signOut()">
-                        Sign out
+                        Sign Out
                     </a> -
-                    <a class="a-button" onclick="window.location='mailto:sam@developersam.com'">
-                        Bug report
+                    <a class="a-button" onclick="window.location='mailto:jedi.primer@yandex.com'">
+                        Bug Report
                     </a>
                 </div>
         </div>
@@ -223,54 +259,7 @@ if (!function_exists('checkForceQuit')){
 </body>
 <script>
     var VerticalHeight = -40;
-    // draw assignment percentage canvas
-    function updatePercentage(perc) {
-        perc = parseFloat(perc);
-        em = Number(getComputedStyle(document.body, null).fontSize.replace(/[^\d]/g, ''));
-
-        var canvas = document.getElementById("percentage");
-        var ctx = canvas.getContext("2d");
-        var text = parseInt(perc * 100) + '%';
-
-        canvas.width = 40 * em;
-        canvas.height = 40 * em;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.lineWidth = 25;
-
-        if (perc == 1.0) {
-            ctx.beginPath();
-            ctx.arc(20 * em, 20 * em, 12 * em, 0, 2 * Math.PI);
-            ctx.strokeStyle = 'rgba(81,157,217,1)';
-            ctx.stroke();
-        }
-
-        if (perc == 0.0) {
-            ctx.beginPath();
-            ctx.arc(20 * em, 20 * em, 12 * em, 0, 2 * Math.PI);
-            ctx.strokeStyle = 'rgba(240,124,120,1)';
-            ctx.stroke();
-        }
-
-        if ((perc < 1) && (perc > 0)) {
-            ctx.beginPath();
-            ctx.arc(20 * em, 20 * em, 12 * em, (1.0125 - perc) * 2 * Math.PI, 2 * Math.PI - 0.075);
-            ctx.strokeStyle = 'rgba(81,157,217,1)';
-            ctx.stroke();
-
-            ctx.beginPath();
-            ctx.strokeStyle = 'rgba(240,124,120,1)';
-            ctx.arc(20 * em, 20 * em, 12 * em, -0.9875 * 2 * Math.PI, -(perc + 0.0125) * 2 * Math.PI);
-            ctx.stroke();
-        }
-
-        ctx.font = 8 * em + "px Arial";
-        ctx.fillStyle = 'rgba(133,189,234,1)';
-        ctx.fillText(text, (canvas.width - text.length*em*4)/2 - 25, (canvas.height + 4*em)/2);
-    }
-
-    // other functions
-    var featureList = ["add-activity", "add-activity-comment", "view-activity-members"];
-    var floatBox = new FloatBox(featureList);
+    
     function loadAssignment(func){
         $.get("/modules/assignment/studentLoadAssignment.php",function(data){
             func();
@@ -301,7 +290,7 @@ if (!function_exists('checkForceQuit')){
                 }
                 var assignment = new Assignment("student", row.id, row.type, row.content, row.attachment, row.publish, row.dueday, subject, row.duration, row.finished, row.class);
                 $('#assignment-list').append(assignment.getHTML(true));
-                VerticalHeight += 189; // 173 is the height of the card.
+                VerticalHeight += 189; // 189 is the height of the card.
             }
             if (todayTotalTime == 0){
                 todayDoneTime = 1;
@@ -323,138 +312,11 @@ if (!function_exists('checkForceQuit')){
             VerticalHeight = -40;
         });
     }
-    function isNull(t){
-        return (t == null || t == "");
-    }
-    function hasFile(id){
-        //if there is a value, return true, else: false;
-        return $('#'+id).val() ? true: false;
-    }
-    $('#submit_form_add_activity').submit(function(){
-        $(this).ajaxSubmit({
-            beforeSubmit: function(){
-                if (isNull($("#add-activity-form-name").val())){
-                    alert("Name of the activity cannot be empty!");
-                    return false;
-                }
-                if (isNull($("#add-activity-form-description").val())){
-                    alert("Description of the activity cannot be empty!");
-                    return false;
-                }
-                $('#submit_btn_add_activity').prop('disabled',true).val("Sending...");
-                $("#progress_activity_1").show();
-                return true;
-            },
-            uploadProgress: function(event, position, total, percentComplete) {
-                $('#progress_activity_1').attr("value", percentComplete);
-            },
-            clearForm: true,
-            data:{hasAttachment: hasFile('add-activity-form-file')},
-            success: function(content,textStatus,xhr,$form){
-                if (content == "Success"){
-                    alert(content);
-                }
-                $('#submit_btn_add_activity').prop('disabled',false).val("Submit");
-                $('#shadow').hide();
-                $("#progress_activity_1").hide();
-                new ManipulateActivity().loadActivities(function(){
-                    $('#activity-list').html("");
-                })
-            }
-        });
-        return false;
-    });
-    $('#submit_form_add_activityComment').submit(function(){
-        $(this).ajaxSubmit({
-            beforeSubmit: function(){
-                if (isNull($("#add-activityComment-form-comment").val())){
-                    alert("Comment cannot be empty!");
-                    return false;
-                }
-                $('#submit_btn_add_activityComment').prop('disabled',true).val("Sending...");
-                $("#progress_activity_2").show();
-                return true;
-            },
-            uploadProgress: function(event, position, total, percentComplete) {
-                $('#progress_activity_2').attr("value", percentComplete);
-            },
-            clearForm: true,
-            data:{hasAttachment: hasFile('add-activityComment-form-file')},
-            success: function(content,textStatus,xhr,$form){
-                if (content == "Success"){
-                    alert(content);
-                }
-                $('#submit_btn_add_activityComment').prop('disabled',false).val("Submit");
-                $('#shadow').hide();
-                $("#progress_activity_2").hide();
-                var id = $('#right-part-view-activity-id').html();
-                new Activity(id, "", "", "", "", "", "", "", [], []).loadComments(function(){
-                    $('#activity-comment-list').html("");
-                });
-            }
-        });
-        return false;
-    });
-    $('#add-activity-form-file-button-1').click(function(e){
-        e.preventDefault();
-        var html = "<div class='mdl-textfield mdl-js-textfield' style='display: block; padding: 10px 0; width: 100%'><input class='mdl-textfield__input uploadfile1' style='margin-top: 0.5em; background: white' name='attachment[]' type='file' multiple /></div>";
-        $("#add-activity-form-file-input-list").append(html);
-    });
-    $('#add-activityComment-form-file-button').click(function(e){
-        e.preventDefault();
-        var html = "<div class='mdl-textfield mdl-js-textfield' style='display: block; padding: 10px 0; width: 100%'><input class='mdl-textfield__input uploadfile1' style='margin-top: 0.5em; background: white' name='attachment[]' type='file' multiple /></div>";
-        $("#add-activityComment-form-file-input-list").append(html);
-    });
-    toggleModules('Home');
     loadAssignment(function(){
         $('#assignment-list').html("");
-    });
-    new ManipulateActivity().loadActivities(function(){
-        $('#activity-list').html("");
-    });
-    new ManipulateCollege().loadColleges(function(){
-        $('#college-list').html("");
     });
     new Class('', '').loadClass(1, function(){
         $('#classList').html("");
     });
-    new ManipulatePresentation().loadPresentations(1);
-
-    // set app state change
-    function showAssignment(){
-        $('#assignment').css('display','block');
-        $('#classes').css('display','none');
-        $('#settings').css('display','none');
-
-        $('#app-assignment').css('display','none');
-        $('#app-classes').css('display','inline-block');
-        $('#app-settings').css('display','inline-block');
-
-        showApps();
-    }
-
-    function showClasses(){
-        $('#assignment').css('display','none');
-        $('#classes').css('display','block');
-        $('#settings').css('display','none');
-
-        $('#app-assignment').css('display','inline-block');
-        $('#app-classes').css('display','none');
-        $('#app-settings').css('display','inline-block');
-
-        showApps();
-    }
-
-    function showSettings(){
-        $('#assignment').css('display','none');
-        $('#classes').css('display','none');
-        $('#settings').css('display','block');
-
-        $('#app-assignment').css('display','inline-block');
-        $('#app-classes').css('display','inline-block');
-        $('#app-settings').css('display','none');
-
-        showApps();
-    }
 </script>
 </html>
