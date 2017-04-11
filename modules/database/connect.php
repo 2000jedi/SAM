b99a56ec5509bcedc53aee1d967c74c83b8adc7e
David Zhong <davidzyc2000@hotmail.com>
2017-04-11 20:33:15 +0800

    DO NOT USE IN PRODUCTION
    Remove mysql_real_escape_string to make SAM compatible with PHP7.
    SQL Injection Protection is now DISABLED now. TODO: Replace all SQL
    sentence with PDO or MYSQLi.

--- modules/database/connect.php
+++ modules/database/connect.php
@@ -26,21 +26,23 @@ if ($mode == "local"){
         die("Connection failed: " . $conn->connect_error);
     }
 
-    foreach ($_POST as $db_post_1_1 => $db_post_2_1) {
+/***    foreach ($_POST as $db_post_1_1 => $db_post_2_1) {
         $_POST[$db_post_1_1] = mysql_real_escape_string($db_post_2_1);
     }
     foreach ($_GET as $db_post_1_2 => $db_post_2_2) {
         $_GET[$db_post_1_2] = mysql_real_escape_string($db_post_2_2);
     }
+***/
 }else if ($mode == "SAE"){
     $conn = new mysqli(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS,"app_wflmssam");
-
+/***
     foreach ($_POST as $db_post_1_1 => $db_post_2_1) {
         $_POST[$db_post_1_1] = addslashes($db_post_2_1);
     }
     foreach ($_GET as $db_post_1_2 => $db_post_2_2) {
         $_GET[$db_post_1_2] = addslashes($db_post_2_2);
     }
+***/
 }
 
 
