72b376dec501175b357c79b987f1734f649ee4fe
Sam Chou <cfkillerusn#gmail.com>
2017-05-13 10:34:01 +0800

    Polished the Contacts feature
    Added some animations

--- template/scripts/contacts.js
+++ template/scripts/contacts.js
@@ -27,25 +27,30 @@ function loadAllPersonalInfo(){
 			var classPersonalInfoArray = oneClass.personalInfoArr;
 			html += "<div class='card'>";
         	html += "	<h2 class=''>" + className +"</h2>";
-        	html += "   <div><a class='a-button' onclick='ExpandORCollaseClass("+className+")'>Expand/Collase</a></div>";
-        	html += "</div>";
-			html += "<div id='info-list-container-"+className+"' style='padding: 0 5px; position: relative; display: none;background-color: #edeff1;'>"
+        	html += "   <div><a class='a-button' onclick='ExpandORCollapseClass("+className+")'>Expand/Collapse</a></div>";
+			html += "	<div id='info-list-container-"+className+"' class='heightTransitionHide' style='padding: 0 5px; position: relative; overflow: hidden'>"
 			for (var j = 0; j < classPersonalInfoArray.length; j++) {
 				var onePerson = classPersonalInfoArray[j];
 				var cName = onePerson.ChineseName;
 				var eName = onePerson.EnglishName;
 				var info = Utils.string.formattedPostContent(onePerson.info);
-				html += "<div class='card info-list-" + className + "'>";
+				html += "<div class='card info-list-" + className + "' style='width: auto'>";
         		html += "	<h4 class=''>" + cName + " ("  + eName + ")</h2>";
         		html += "   <div>" + info +"</div>";
         		html += "</div>";
 			}
+			html += "	</div>";
 			html += "</div>"
 		}
 		$('#contactsList').html(html);
 	})
 }
 
-function ExpandORCollaseClass(classID){
-	$("#info-list-container-"+classID).slideToggle();
+function ExpandORCollapseClass(classID){
+	var container = $("#info-list-container-"+classID);
+	if ( container.hasClass("heightTransitionHide")) {
+		container.addClass("heightTransitionShow").removeClass("heightTransitionHide");
+	}else{
+		container.addClass("heightTransitionHide").removeClass("heightTransitionShow");
+	}
 }
